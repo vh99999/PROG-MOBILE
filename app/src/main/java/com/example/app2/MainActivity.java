e4be2694a8cabe661ace3ef35d24b75a5f14208c
Vitor Hugo Prange <vitor.p2007@aluno.ifsc.edu.br>
2025-08-11 09:09:21 -0300

    contador não funcional

--- app/src/main/java/com/example/app2/MainActivity.java
+++ app/src/main/java/com/example/app2/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.app2;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    int cont = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +25,15 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        textView=findViewById(R.id.tv);
+        textView.setText(Integer.toString(cont));
+        textView.setOnClickListener((View v)-> {
+            cont++;
+            textView.setText(cont);
+        });
+
+
+
+
     }
 }

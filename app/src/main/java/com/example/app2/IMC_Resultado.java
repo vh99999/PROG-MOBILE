be66f7041a5a5f4420d7457a8e374204db748960
Prange <prange.vitor@gmail.com>
2025-09-08 09:16:50 -0300

    Botão funcional

--- app/src/main/java/com/example/app2/IMC_Resultado.java
+++ app/src/main/java/com/example/app2/IMC_Resultado.java
@@ -1,9 +1,6 @@
 package com.example.app2;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,20 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
-    Button button;
+public class IMC_Resultado extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        button = findViewById(R.id.button4);
-        button.setOnClickListener( v ->{
-            Intent intent = new Intent(this, IMC_Resultado.class);
-            startActivity(intent);
-        });
+        setContentView(R.layout.activity_imc_resultado);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);

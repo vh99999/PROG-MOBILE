3609ccc45f0728d5d73eb306a40072c0b2b394d3
Prange <vitor.p2007@aluno.ifsc.edu.br>
2025-11-03 09:13:04 -0300

    SQLite inicio

--- app/src/main/java/com/example/app2/MainActivity.java
+++ app/src/main/java/com/example/app2/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.app2;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +15,34 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    SQLiteDatabase db;
+    Button salvar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        salvar = findViewById(R.id.button);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        db = openOrCreateDatabase("mdb.db", MODE_PRIVATE, null);
+        db.execSQL("CREATE TABLE IF NOT EXISTS notas (id INTEGER PRIMARY KEY AUTOINCREMENT, titulo VARCHAR, texto TEXT)");
+
+        salvar.setOnClickListener(v -> {
+            EditText et = findViewById(R.id.editTextText);
+            String texto = et.getText().toString();
+            ContentValues cv = new ContentValues();
+            cv.put("titulo", "Mi primera nota");
+            cv.put("texto", "Texto de la nota");
+            db.insert("notas", null, cv);
+            Toast.makeText(this, "Butes", Toast.LENGTH_SHORT).show();
+
+        });
+
     }
 }
